ac36608136d1be97a1ea6aa04a96b4f1ce19e5ef
Andres <aparrag@cifpfbmoll.eu>
2022-01-14 13:51:40 +0100

    Initial commit

---
+++ app/src/main/java/com/example/game/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.GridLayout;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    GridLayout grid;
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        grid = findViewById(R.id.grid);
+
+        grid.setOnTouchListener(new OnSwipeTouchListener(MainActivity.this) {
+            public void onSwipeTop() {
+                Toast.makeText(MainActivity.this, "top", Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeRight() {
+                Toast.makeText(MainActivity.this, "right", Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeLeft() {
+                Toast.makeText(MainActivity.this, "left", Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeBottom() {
+                Toast.makeText(MainActivity.this, "bottom", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
